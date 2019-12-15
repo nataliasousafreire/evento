021451628a4c97e369cc688ee77ff66a98e77902
MiguelCastro1 <miguelcastro0207@gmail.com>
2019-12-15 04:24:16 -0400

    Atualizacao no Banco

---
+++ models/menu.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+# ----------------------------------------------------------------------------------------------------------------------
+# this is the main application menu add/remove items as required
+# ----------------------------------------------------------------------------------------------------------------------
+
+response.menu = [
+    (T('Home'), False, URL('default', 'home'), []),
+    (T('Meus Eventos'), False, URL('default', 'meus_eventos'), []),
+    (T('Criar Evento'), False, URL('default', 'criar_evento'), []),
+    (T('Meus Perfil'), False, URL('default', 'user', 'profile'), []),
+    (T('Relatorio Tags'), False, URL('default', 'tag'), []),
+    (T('Relatorio Clientes'), False, URL('default', 'cliente'), []),
+    (T('Relatorio Intervalos'), False, URL('default', 'intervalo'), [])
+]
+
